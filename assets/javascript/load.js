47d899740edbaa5e320e48a959ef0b6e9dba93b6
Trys <trysmudford@gmail.com>
2014-03-06 23:15:20 +0000

    Github Pages branch

---
+++ assets/javascript/load.js
@@ -0,0 +1,32 @@
+var trys = (function () {
+	"use strict";
+
+	return {
+
+		init: function () {
+
+			$(window).scroll(function(){
+				trys.Animation.Parallax();
+			});
+
+		},
+
+		Animation : {
+
+			Parallax : function () {
+				var scrollPos = $(window).scrollTop();
+				$('.entry-title').css( 'top', + ( scrollPos * 0.4 ) + 'px' );
+
+				if( scrollPos > ( $('#hero').outerHeight() - 66 ) && ! $('.site-title').hasClass('title-visible') ) {
+					console.log(true);
+					$('.site-title').addClass('title-visible');
+				}
+
+			}
+
+		}
+
+	};
+}());
+
+$( trys.init() );
