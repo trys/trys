38f3ad59c9d835353e57f462f6f70e6103fb9fe6
Trys <trysmudford@gmail.com>
2014-03-08 10:02:08 +0000

    Javascript clean up

--- assets/javascript/load.js
+++ assets/javascript/load.js
@@ -1,31 +1,20 @@
 var trys = (function () {
 	"use strict";
-
 	return {
-
 		init: function () {
-
 			$(window).scroll(function(){
 				trys.Animation.Parallax();
 			});
-
 		},
-
 		Animation : {
-
 			Parallax : function () {
 				var scrollPos = $(window).scrollTop();
 				$('.entry-title').css( 'top', + ( scrollPos * 0.4 ) + 'px' );
-
 				if( scrollPos > ( $('#hero').outerHeight() - 66 ) && ! $('.site-title').hasClass('title-visible') ) {
-					console.log(true);
 					$('.site-title').addClass('title-visible');
 				}
-
 			}
-
 		}
-
 	};
 }());
 
