d788fa37188b9f040c3fa8086b0f1f8417807cf4
Trys <trysmudford@gmail.com>
2014-03-12 21:01:15 +0000

    Moved google analytics out of the footer

--- assets/javascript/load.js
+++ assets/javascript/load.js
@@ -19,3 +19,13 @@ var trys = (function () {
 }());
 
 $( trys.init() );
+
+/* Google Analytics */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-33133833-1', 'trysmudford.com');
+ga('send', 'pageview');
